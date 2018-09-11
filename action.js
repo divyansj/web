ba16cac8287fcbf48d0108108baec15159540cf7
divyansj <divyansj@gmail.com>
2018-09-11 13:34:28 +0530

    Add files via upload

--- action.js
+++ action.js
@@ -50,11 +50,10 @@ var qwe;
 var bum = 0;
 var slideIndex = 0;
 var slides, square;
-round(0);
+revolve(slideIndex);
 fun(1);
 
-function myfunction(num, el) {
-
+function myfunction(num,el) {
   $('.seconbutton').removeClass('active').filter($(el)).addClass('active');
   $('.section').hide();
   $('#main' + num).show();
@@ -62,116 +61,124 @@ function myfunction(num, el) {
 //////////////////////////////////////////////////////////////////////////////////
 function currentslide(tum) {
   slideIndex = tum;
-  round(1);
+  bum = 1;
+  revolve(slideIndex);
 }
 //////////////////////////////////////////////////////////////////////////////////
 function plusslide(slidenumchange) {
   slideIndex += slidenumchange;
-  round(1);
+  bum = 1;
+  revolve(slideIndex);
 }
 //////////////////////////////////////////////////////////////////////////////////
-function round(waitt) {
+/*function round(waitt) {
   if (waitt == 0) {
     bum = 0;
-  //  clearTimeout(rty);
+    revolve();
     qwe = setTimeout(revolve, 2000);
     clearTimeout(rty);
+    round(0);
   } else {
     waitt = 0;
     bum = 1;
-    //clearTimeout(qwe);
-    rty = setTimeout(revolve, 5000);
+    revolve();
+    rty = setTimeout(function(){}, 5000);
     clearTimeout(qwe);
-
+    round(0);
   }
-}
+}*/
 /////////////////////////////////////////////////////////////////////////////////
-function revolve() {
+function revolve(slideIndex) {
   var i;
   slides = document.getElementsByClassName("mySlides");
   square = document.getElementsByClassName("squares");
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
   if (bum != 0) {
+    clearTimeout(rty);
     if (slideIndex < 1) {
       slideIndex = slides.length;
     }
   }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
   if (bum == 0) {
     slideIndex++;
+    clearTimeout(qwe);
   }
-
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
   for (i = 0; i < square.length; i++) {
     square[i].className = square[i].className.replace(" showing", "");
-
   }
   slides[slideIndex - 1].style.display = "block";
   square[slideIndex - 1].className += " showing";
-  round(0);
-
+  if (bum != 0) {
+    bum = 0;
+    qwe = setTimeout(revolve, 5000, slideIndex);
+  } else {
+    bum = 0;
+    rty = setTimeout(revolve, 2000, slideIndex);
+  }
 }
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 function fun(num) {
   var acc = document.getElementsByClassName("accordion");
   var pan = document.getElementsByClassName("panel");
   var i;
   num--;
-
   for (i = 0; i < acc.length; i++) {
     acc[i].className = acc[i].className.replace(" open", "");
-    if (pan[num].style.maxHeight)
-      continue;
-    else
-      pan[i].style.maxHeight = null;
+    if (pan[num].style.maxHeight) continue;
+    else pan[i].style.maxHeight = null;
   }
   acc[num].className += " open";
-
   if (pan[num].style.maxHeight) {
     pan[num].style.maxHeight = null;
   } else {
     pan[num].style.maxHeight = pan[num].scrollHeight + "px";
   }
-
 }
 ///////////////////////////////////////////////////////////////////////////////////
 var modal = document.getElementById('myModal');
-
-
 var btn = document.getElementById("myBtn");
-
 var span = document.getElementsByClassName("close")[0];
 var sub = document.getElementById('submitchange');
-
 btn.onclick = function() {
   modal.style.display = "block";
   $('#changething').trigger("reset");
 }
-
 span.onclick = function() {
-  modal.style.display = "none";
+	if (((document.getElementById("headingchange").value) || (document.getElementById("parachange").value)) == "")
+	document.getElementById("inputman").innerHTML = "Please enter both values";
+	else {  modal.style.display = "none";}
 }
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
-
-sub.onclick = function() {
-
+window.onload = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+/*sub.onclick = function() {
+  if(((document.getElementById("headingchange").value) || (document.getElementById("parachange").value) )=="")
+  document.getElementById("#inputman").innerHTML="comeon man";
+else
   modal.style.display = "none";
 
-}
-
+}*/
 function changethings() {
-  document.getElementById("changehere").innerHTML = document.getElementById("headingchange").value;
-  document.getElementById("bookpara").innerHTML = document.getElementById("parachange").value;
-
+  if (((document.getElementById("headingchange").value) || (document.getElementById("parachange").value)) == "")
+	document.getElementById("inputman").innerHTML = "Please enter both values";
+  else {
+    document.getElementById("changehere").innerHTML = document.getElementById("headingchange").value;
+    document.getElementById("bookpara").innerHTML = document.getElementById("parachange").value;
+    modal.style.display = "none";
+		document.getElementById("inputman").innerHTML = "";
+  }
 }
 $("#changething").submit(function(e) {
   e.preventDefault();
@@ -181,11 +188,9 @@ function previewFile() {
   var preview = document.getElementById('bookimage');
   var file = document.getElementById('imagechange').files[0];
   var reader = new FileReader();
-
   reader.onloadend = function() {
     preview.src = reader.result;
   }
-
   if (file) {
     reader.readAsDataURL(file);
   } else {
